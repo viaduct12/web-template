72c20405c41f5a4e4ea47d421f2edc9add0320bd
Lawrence Fiesta <lfiesta86@gmail.com>
2019-11-30 15:00:20 -0800

    finished to modal, need to add a form to the modal. also need to deploy

--- client/src/components/Modal/index.jsx
+++ client/src/components/Modal/index.jsx
@@ -1,48 +1,59 @@
 import React, { Component } from "react";
+import './style.css';
 
+const display = {
+  display: "block"
+};
 
-  const display = {
-    display: "block"
-  };
-
-  const hide = {
-    display: 'none'
-  };
+const hide = {
+  display: "none"
+};
 export default class Modal extends Component {
-
-  state = {
-    toggle: 'false'
+  constructor(props) {
+    super(props);
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      toggle: false
+    };
   }
-
-  // toggle(e) {
-  //   this.setState(prevState => {
-  //     toggle: !prevState.toggle
-  //   })
-  // }
+  toggle(event) {
+    this.setState(prevState => ({
+      toggle: !prevState.toggle
+    }));
+  }
   render() {
+    const modal = [];
+    modal.push(
+      <div
+        id="modal1"
+        className="modal modal-fixed-footer"
+        style={this.state.toggle ? display : hide} 
+      >
+        <div className="modal-content">
+          <h4>Agenda</h4>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus necessitatibus eveniet sit culpa, animi repellat provident debitis modi fugiat sed? Libero quod nam placeat minima reiciendis nesciunt recusandae soluta dolores.</p>
+        </div>
+        <div className="modal-footer">
+          <a
+            href="#!"
+            className="modal-close waves-effect waves-green btn-flat"
+            onClick={this.toggle}
+          >
+            Agree
+          </a>
+        </div>
+      </div>
+    );
     return (
       <div>
         <a
           className="waves-effect waves-light btn modal-trigger"
           href="#modal1"
+          onClick={this.toggle}
         >
           Post
         </a>
-
-        <div id="modal1" className="modal modal-fixed-footer">
-          <div className="modal-content">
-            <h4>Modal Header</h4>
-            <p>A bunch of text</p>
-          </div>
-          <div className="modal-footer">
-            <a
-              href="#!"
-              className="modal-close waves-effect waves-green btn-flat"
-            >
-              Agree
-            </a>
-          </div>
-        </div>
+        {modal}
       </div>
     );
   }
