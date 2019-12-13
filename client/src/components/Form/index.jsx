875ea60d3de563a47db8d0f66169304ed387c0db
Lawrence Fiesta <lfiesta86@gmail.com>
2019-12-12 20:33:08 -0800

    added drop down to the post modal

--- client/src/components/Form/index.jsx
+++ client/src/components/Form/index.jsx
@@ -3,6 +3,16 @@
 import React, { Component } from "react";
 
 export default class Form extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { value: "select_school" };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
   render() {
     return (
       <div>
@@ -10,57 +20,32 @@ export default class Form extends Component {
           <form className="col s12">
             <div className="row">
               <div className="input-field col s6">
-                <input
-                  placeholder="Placeholder"
-                  id="first_name"
-                  type="text"
-                  className="validate"
-                />
-                <label for="first_name">First Name</label>
+                <input id="user_name" type="text" className="validate" />
+                <label for="user_name">Username</label>
+                {/*pulls user name from db */}
               </div>
               <div className="input-field col s6">
-                <input id="last_name" type="text" className="validate" />
-                <label for="last_name">Last Name</label>
+                <select className="browser-default" value={this.state.value} onChange={this.handleChange}>
+                  <option value="select_school" disabled>Select school</option>
+                  <option value="seattle">Seattle</option>
+                  <option value="bothell">Bothell</option>
+                  <option value="tacoma">Tacoma</option>
+                </select>
               </div>
             </div>
             <div className="row">
               <div className="input-field col s12">
-                <input
-                  disabled
-                  value="I am not editable"
-                  id="disabled"
-                  type="text"
-                  className="validate"
-                />
-                <label for="disabled">Disabled</label>
+                <input id="title" type="text" className="validate" />
+                <label for="title">Title</label>
               </div>
             </div>
             <div className="row">
               <div className="input-field col s12">
-                <input id="password" type="password" className="validate" />
-                <label for="password">Password</label>
-              </div>
-            </div>
-            <div className="row">
-              <div className="input-field col s12">
-                <input id="email" type="email" className="validate" />
-                <label for="email">Email</label>
-              </div>
-            </div>
-            <div className="row">
-              <div className="col s12">
-                This is an inline input field:
-                <div className="input-field inline">
-                  <input id="email_inline" type="email" className="validate" />
-                  <label for="email_inline">Email</label>
-                  <span
-                    className="helper-text"
-                    data-error="wrong"
-                    data-success="right"
-                  >
-                    Helper text
-                  </span>
-                </div>
+                <textarea
+                  id="textarea1"
+                  className="materialize-textarea"
+                ></textarea>
+                <label for="textarea1">Description</label>
               </div>
             </div>
           </form>
